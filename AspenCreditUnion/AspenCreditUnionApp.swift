9974ffb0d4a0af645a9d24aa0c15e6c75ac0a47d
Jonathan <jonathanmccormick@users.noreply.github.com>
2025-04-24 18:30:27 -0600

    Initial commit of Aspen Credit Union project, including main app structure, assets, content view, and test cases.

---
+++ AspenCreditUnion/AspenCreditUnionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AspenCreditUnionApp.swift
+//  AspenCreditUnion
+//
+//  Created by Jonathan McCormick on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct AspenCreditUnionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
